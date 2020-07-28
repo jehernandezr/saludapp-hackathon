163f9a01c4cfd332f4092f84ef03ef49da48e9aa
casalazara <ca.salazara@uniandes.edu.co>
2020-07-28 16:21:46 -0500

    Envía SMS

--- saludapp/api/logic/logic.py
+++ saludapp/api/logic/logic.py
@@ -1,5 +1,7 @@
 from ..models import *
 import datetime
+from twilio.rest import Client
+
 def createCita(cedula,fecha, hora):
 
 
@@ -12,6 +14,18 @@ def createCita(cedula,fecha, hora):
     if cita and usuario:
         medico = cita[0].medico
         reserva = Cita(medico=medico,paciente=usuario,disponible=cita[0])
+        sendSMS(reserva.url,usuario.celular,fecha,hora)
         reserva.save()
         return True
     return False
+
+def sendSMS(link, celular, dia, hora):
+    account_sid = 'ACcc467e48ab7f253e2de98eca1667ad16'
+    auth_token = '1d8e51c2405c99fb74055ad7fdf0513b'
+    client = Client(account_sid, auth_token)
+
+    message = client.messages.create(
+        body=f'Su cita ha sido confirmada para el día {dia} a las {hora}. El link para acceder a ella es: {link}',
+        from_='+17252139009',
+        to='+57'+celular
+    )
