1bc56d5473d2e9b785e0de0b3e7800437f6c96d7
Dianis Caro <dianiscaro@outlook.com>
2020-07-29 11:27:10 -0500

    Videocall space

---
+++ front-AIppointment/src/app/pacient/videocall/videocalluser.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-body',
+  templateUrl: './videocalluser.component.html',
+  styleUrls: ['./videocalluser.component.css']
+})
+export class VideocallUser implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
