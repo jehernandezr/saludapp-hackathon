c507f96c49add862b4dc575a0191711d6efd3bac
casalazara <ca.salazara@uniandes.edu.co>
2020-07-29 08:40:04 -0500

    Ya envía emails (Hace falta cambiar el dominio)

--- saludapp/api/logic/logic.py
+++ saludapp/api/logic/logic.py
@@ -1,9 +1,16 @@
 from ..models import *
 import datetime
 from twilio.rest import Client
+import smtplib, ssl
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+import qrcode
+import os
+from email import encoders
+from email.mime.base import MIMEBase
 
 def createCita(cedula,fecha, hora):
-    
+
     usuario = Paciente.objects.get(cedula=cedula)
     fecha = fecha+' '+hora
     format = '%Y-%d-%m %H:%M'
@@ -12,8 +19,13 @@ def createCita(cedula,fecha, hora):
     cita = Disponible.objects.filter(fecha=fecha).filter(estado='L')
     if cita and usuario:
         medico = cita[0].medico
+
         reserva = Cita(medico=medico,paciente=usuario,disponible=cita[0])
+        setattr(cita, 'estado', 'R')
+        cita.save()
+
         sendSMS(reserva.url,usuario.celular,fecha,hora)
+        sendMail(reserva.url,usuario,fecha,hora)
         reserva.save()
         return True
     return False
@@ -24,7 +36,65 @@ def sendSMS(link, celular, dia, hora):
     client = Client(account_sid, auth_token)
 
     message = client.messages.create(
-        body=f'Su cita ha sido confirmada para el día {dia} a las {hora}. El link para acceder a ella es: {link}',
+        body=f'Tu cita ha sido confirmada para el día {dia} a las {hora}. El link para acceder a ella es: {link}',
         from_='+17252139009',
         to='+57'+celular
     )
+
+def sendMail(link,usuario,dia,hora):
+
+    img = qrcode.make(link)
+    ruta_imagen = f'{usuario.cedula}.png'
+    img.save(ruta_imagen)
+
+    sender_email = "aippointment@gmail.com"
+    receiver_email = usuario.correo
+    password = "HackathonUniandes2020"
+
+    message = MIMEMultipart("alternative")
+    message["Subject"] = "Confirmación cita"
+    message["From"] = sender_email
+    message["To"] = receiver_email
+
+    # Create the plain-text and HTML version of your message
+    html = f"""\
+    <html>
+      <body>
+        <p>Hola {usuario.nombre},<br>
+           Tu cita ha sido confirmada para el día {dia} a las {hora}.,<br>
+           <a href="{link}">Haz click aquí para acceder a tu cita.</a>
+        </p>
+        <p> O escanea el código QR adjunto para acceder a tu cita.
+        </p>
+      </body>
+    </html>
+    """
+
+    # Turn these into plain/html MIMEText objects
+    part2 = MIMEText(html, "html")
+    # Open PDF file in binary mode
+    with open(ruta_imagen, "rb") as attachment:
+        # Add file as application/octet-stream
+        # Email client can usually download this automatically as attachment
+        part = MIMEBase("application", "octet-stream")
+        part.set_payload(attachment.read())
+
+    # Encode file in ASCII characters to send by email
+    encoders.encode_base64(part)
+
+    # Add header as key/value pair to attachment part
+    part.add_header(
+        "Content-Disposition",
+        f"attachment; filename= {ruta_imagen}",
+    )
+
+    message.attach(part)
+    message.attach(part2)
+
+    context = ssl.create_default_context()
+    with smtplib.SMTP_SSL("smtp.gmail.com", 465, context=context) as server:
+        server.login(sender_email, password)
+        server.sendmail(
+            sender_email, receiver_email, message.as_string()
+        )
+    os.remove(ruta_imagen)
